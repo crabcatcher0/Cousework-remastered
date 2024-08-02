461aa31591a24181dee32393781500671634891f
CrabCatcher0 <rautprashant103@gmail.com>
2024-08-02 20:13:52 +0545

    frontend and backend added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import styles from './Navbar.module.css';
+
+function Navbar() {
+    <div className={styles.navbar}>
+        <navbar>
+            <div className={styles.navBrand}>
+                <h2>HOSTEL</h2>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Hostels</li>
+                <li>Signup</li>
+                <li>Contact</li>
+            </ul>
+        </navbar>
+
+    </div>
+
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.css'
+
+function Home() {
+    return(
+        <div className={styles.homepage}>
+            <h1>This is HomePage.</h1>
+        </div>
+    );
+}
+
+export default Home;
